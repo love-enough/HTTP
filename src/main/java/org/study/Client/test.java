c719314fc787b4d18bf431c29c9f07ddcd60bfd0
GuoZihan <guozihan23@mails.ucas.ac.cn>
2024-06-25 13:31:46 +0800

    实现基本的静态资源解析

--- src/main/java/org/study/Client/test.java
+++ src/main/java/org/study/Client/test.java
@@ -2,6 +2,6 @@ package org.study.Client;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("start");
+        System.out.println("start test!");
     }
 }
